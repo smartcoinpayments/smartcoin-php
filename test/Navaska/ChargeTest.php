d72987c83da9a0a69a10925b9de1fe9f5fe8852d
agranado <agranado2k@gmail.com>
2014-02-22 15:52:04 -0300

    Revert "Refactoy: On ChargeTest to reduce code"
    This reverts commit 81046f57ebbe568e25704b4cb75c4b7fea34121d.

--- test/Navaska/ChargeTest.php
+++ test/Navaska/ChargeTest.php
@@ -1,18 +1,14 @@
 <?php
   class Test_Navaska_Charge extends UnitTestCase {
-    $api_keys;
-    $token;
 
-    function setUp() {
+    function test_create_and_retrieve_charge() {
       $api_keys = 'pk_test_0208ca9d84d92a:sk_test_62a57820440d47';
       $params = array('number' => 4242424242424242,
                       'exp_month' => 11,
                       'exp_year' => 2017,
                       'name' => 'Arthur Granado');
       $token = \Navaska\Token::create($params, $api_keys);
-    }
 
-    function test_create_and_retrieve_charge() {
       $params = array(
           'amount' => 1000,
           'currency' => 'brl',
@@ -26,6 +22,13 @@
     }
 
     function test_capture_charge() {
+      $api_keys = 'pk_test_0208ca9d84d92a:sk_test_62a57820440d47';
+      $params = array('number' => 4242424242424242,
+                      'exp_month' => 11,
+                      'exp_year' => 2017,
+                      'name' => 'Arthur Granado');
+      $token = \Navaska\Token::create($params, $api_keys);
+
       $params = array(
           'amount' => 1000,
           'currency' => 'brl',
@@ -40,6 +43,13 @@
     }
 
     function test_refund_charge() {
+      $api_keys = 'pk_test_0208ca9d84d92a:sk_test_62a57820440d47';
+      $params = array('number' => 4242424242424242,
+                      'exp_month' => 11,
+                      'exp_year' => 2017,
+                      'name' => 'Arthur Granado');
+      $token = \Navaska\Token::create($params, $api_keys);
+
       $params = array(
           'amount' => 1000,
           'currency' => 'brl',
