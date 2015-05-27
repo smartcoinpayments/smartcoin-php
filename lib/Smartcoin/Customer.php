4af98d7051237eeee45090596999a23cf4cbc7c2
Arthur Granado <agranado@smartcoin.com.br>
2015-05-27 18:13:49 -0300

    Merge with sergio branch

--- lib/Smartcoin/Customer.php
+++ lib/Smartcoin/Customer.php
@@ -1,49 +1,56 @@
 <?php
 namespace Smartcoin;
 
-  class Customer extends \Smartcoin\Object {
-  	public static function get_request_url() {
-      return "/v1/customers/";
+class Customer extends \Smartcoin\Object
+{
+    public static function get_request_url()
+    {
+        return "/v1/customers/";
     }
 
-    public static function create($params=null) {
-      $url = self::get_request_url();
-      $r = \Smartcoin\APIRequest::request('post',$url, \Smartcoin\Smartcoin::access_keys(), $params);
-      return new Customer(json_decode($r[0],true), \Smartcoin\Smartcoin::access_keys());
+    public static function create($params=null)
+    {
+        $url = self::get_request_url();
+        $r = \Smartcoin\APIRequest::request('post',$url, \Smartcoin\Smartcoin::access_keys(), $params);
+        return new Customer(json_decode($r[0],true), \Smartcoin\Smartcoin::access_keys());
     }
 
-  	public static function retrieve($id=null) {
-      if (strpos($id,'@') !== false)
-        $id = '?email=' . $id;
-      
-  		$url = self::get_request_url();
-  		$r = \Smartcoin\APIRequest::request('get',$url.$id, \Smartcoin\Smartcoin::access_keys());
-      return new Customer(json_decode($r[0],true), \Smartcoin\Smartcoin::access_keys());
-  	}
+    public static function retrieve($id=null)
+    {
+        if (strpos($id,'@') !== false) {
+            $id = '?email=' . $id;
+        }
 
-  	public function subscriptions(){
-  		return new \Smartcoin\Subscription($this->id);
-  	}
-
-    public function update_card($params) {
-      $url = self::get_request_url();
-      $r = \Smartcoin\APIRequest::request('post',$url.$this->id, \Smartcoin\Smartcoin::access_keys(), $params);
-      $this->refresh_object(json_decode($r[0],true),$this->api_keys); 
+        $url = self::get_request_url();
+        $r = \Smartcoin\APIRequest::request('get',$url.$id, \Smartcoin\Smartcoin::access_keys());
+        return new Customer(json_decode($r[0],true), \Smartcoin\Smartcoin::access_keys());
     }
 
-    public function save() {
-      $url = self::get_request_url();
-      $params = $this->serializeParamenters();
-      $r = \Smartcoin\APIRequest::request('post',$url.$this->id, \Smartcoin\Smartcoin::access_keys(), $params);
-      $this->refresh_object(json_decode($r[0],true),$this->api_keys);  
+    public function subscriptions()
+    {
+        return new \Smartcoin\Subscription($this->id);
     }
 
-    public function delete() {
-      $url = self::get_request_url();
-      $r = \Smartcoin\APIRequest::request('delete',$url.$this->id, \Smartcoin\Smartcoin::access_keys());
-      $this->refresh_object(json_decode($r[0],true),$this->api_keys);
-      return json_decode($r[0],true); 
+    public function update_card($params)
+    {
+        $url = self::get_request_url();
+        $r = \Smartcoin\APIRequest::request('post',$url.$this->id, \Smartcoin\Smartcoin::access_keys(), $params);
+        $this->refresh_object(json_decode($r[0],true),$this->api_keys);
     }
-  }
 
-?>
+    public function save()
+    {
+        $url = self::get_request_url();
+        $params = $this->serializeParamenters();
+        $r = \Smartcoin\APIRequest::request('post',$url.$this->id, \Smartcoin\Smartcoin::access_keys(), $params);
+        $this->refresh_object(json_decode($r[0],true),$this->api_keys);
+    }
+
+    public function delete()
+    {
+        $url = self::get_request_url();
+        $r = \Smartcoin\APIRequest::request('delete',$url.$this->id, \Smartcoin\Smartcoin::access_keys());
+        $this->refresh_object(json_decode($r[0],true),$this->api_keys);
+        return json_decode($r[0],true);
+    }
+}
--- lib/Smartcoin/Error.php
+++ lib/Smartcoin/Error.php
@@ -1,42 +1,55 @@
 <?php
-  namespace Smartcoin;
-  class Error extends \Exception {
+namespace Smartcoin;
 
-    public function __construct($message=null, $http_status=null, $http_body=null, $json_body=null) {
-      parent::__construct($message);
-      $this->http_status = $http_status;
-      $this->http_body = $http_body;
-      $this->json_body = $json_body;
+class Error extends \Exception
+{
+    public function __construct(
+        $message = null,
+        $http_status = null,
+        $http_body = null,
+        $json_body = null
+    ) {
+        parent::__construct($message);
+        $this->http_status = $http_status;
+        $this->http_body = $http_body;
+        $this->json_body = $json_body;
     }
 
-    public function get_http_status() {
-      return $this->http_status;
+    public function get_http_status()
+    {
+        return $this->http_status;
     }
 
-    public function get_http_body() {
-      return $this->http_body;
+    public function get_http_body()
+    {
+        return $this->http_body;
     }
 
-    public function get_json_body() {
-      return $this->json_body;
+    public function get_json_body()
+    {
+        return $this->json_body;
     }
-  }
+}
 
-  class AuthenticationError extends Error {
+class AuthenticationError extends Error
+{
 
-  }
+}
 
-  class RequestError extends Error {
+class RequestError extends Error
+{
 
-  }
+}
 
-  class InvalidArgumentException extends \Exception {
-    public function __construct($message=null) {
-      $this->message = $message;
+class InvalidArgumentException extends \Exception
+{
+    public function __construct($message = null)
+    {
+        $this->message = $message;
     }
 
-    public function message() {
-      return $this->message;
+    public function message()
+    {
+        return $this->message;
     }
-  }
-?>
+}
